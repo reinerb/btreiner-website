4680dc201ed37f0f6f02e7687e59a15f09c23299
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-12 15:41:55 -0400

    Add PrimaryLayout to the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import PrimaryLayout from '@/components/PrimaryLayout';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+    <PrimaryLayout>
       <h1>Hello world!</h1>
-    </main>
+    </PrimaryLayout>
   );
 }
