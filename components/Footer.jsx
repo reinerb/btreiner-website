32ff2c54b7aac2f5fc75db35d5a4fceac6e0c693
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-12 17:02:25 -0400

    Style footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import SocialIcons from './SocialIcons';
 
 const Footer = () => {
-  return <footer>Hello world</footer>;
+  return (
+    <footer className='flex flex-col items-center gap-3 bg-zinc-950 md:px-6 md:py-3 px-4 py-2'>
+      <p className='text-zinc-200'>Copyright &copy; Benjamin Reiner, 2023.</p>
+      <SocialIcons />
+    </footer>
+  );
 };
 
 export default Footer;
--- components/PrimaryLayout.jsx
+++ components/PrimaryLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const PrimaryLayout = ({ children }) => {
   return (
-    <>
+    <div className='min-h-screen grid grid-rows-site-layout'>
       <Header />
       <main className='container mx-auto my-4 px-6'>{children}</main>
-    </>
+      <Footer />
+    </div>
   );
 };
 
