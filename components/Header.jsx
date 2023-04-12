bfb550c84f47c6a8b39100bb2b2c489a1f8f185c
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-12 16:16:19 -0400

    Style header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,22 +3,27 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
+    <header className='flex flex-col md:flex-row md:justify-between items-center gap-2 bg-zinc-950 md:px-6 md:py-3 px-4 py-2'>
       <div>
-        <h1>Benjamin Reiner</h1>
+        <Link
+          href='/'
+          className='hover:text-zinc-200'
+        >
+          <h1 className='text-2xl'>Benjamin Reiner</h1>
+        </Link>
       </div>
       <nav>
-        <ul>
-          <li>
+        <ul className='flex gap-4'>
+          <li className='hover:text-zinc-200'>
             <Link href='/'>Home</Link>
           </li>
-          <li>
+          <li className='hover:text-zinc-200'>
             <Link href='/about'>About</Link>
           </li>
-          <li>
+          <li className='hover:text-zinc-200'>
             <Link href='/projects'>Projects</Link>
           </li>
-          <li>
+          <li className='hover:text-zinc-200'>
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
