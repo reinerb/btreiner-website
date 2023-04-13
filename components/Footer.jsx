9c0fdac5b8c24b45cddb36b71301b6ea12a6fa93
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-13 15:18:00 -0400

    Add light/dark mode accomodations

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,8 @@ import SocialIcons from './SocialIcons';
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col items-center gap-3 bg-zinc-950 md:px-6 md:py-3 px-4 py-2'>
-      <p className='text-zinc-200'>Copyright &copy; Benjamin Reiner, 2023.</p>
+    <footer className='flex flex-col items-center gap-3 text-zinc-950 dark:text-zinc-50 bg-zinc-200 dark:bg-zinc-950 md:px-6 md:py-3 px-4 py-2'>
+      <p>Copyright &copy; Benjamin Reiner, 2023.</p>
       <SocialIcons />
     </footer>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className='flex flex-col md:flex-row md:justify-between items-center gap-2 bg-zinc-950 md:px-6 md:py-3 px-4 py-2'>
+    <header className='flex flex-col md:flex-row md:justify-between items-center gap-2 bg-zinc-200 dark:bg-zinc-950 dark:text-zinc-50 md:px-6 md:py-3 px-4 py-2'>
       <div>
         <Link
           href='/'
-          className='hover:text-zinc-200'
+          className='hover:text-zinc-700 dark:hover:text-zinc-200'
         >
           <h1 className='text-2xl'>Benjamin Reiner</h1>
         </Link>
       </div>
       <nav>
         <ul className='flex gap-4'>
-          <li className='hover:text-zinc-200'>
+          <li className='hover:text-zinc-700 dark:hover:text-zinc-200'>
             <Link href='/'>Home</Link>
           </li>
-          <li className='hover:text-zinc-200'>
+          <li className='hover:text-zinc-700 dark:hover:text-zinc-200'>
             <Link href='/about'>About</Link>
           </li>
-          <li className='hover:text-zinc-200'>
+          <li className='hover:text-zinc-700 dark:hover:text-zinc-200'>
             <Link href='/projects'>Projects</Link>
           </li>
-          <li className='hover:text-zinc-200'>
+          <li className='hover:text-zinc-700 dark:hover:text-zinc-200'>
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
