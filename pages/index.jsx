67b9400e77b3f60abcdec8a1090fe43a81457407
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-26 15:05:07 -0400

    Add some temporary text to incomplete pages.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,38 @@
 import PrimaryLayout from '@/components/PrimaryLayout';
+import Link from 'next/link';
 
 const Home = () => {
+  const linkStyles =
+    'text-sky-700 dark:text-sky-300 visited:text-sky-600 dark:visited:text-sky-400 underline hover:no-underline';
+
   return (
     <PrimaryLayout>
-      <h1>Home</h1>
+      <h1 className='text-2xl mb-3'>Hi! I'm Ben Reiner.</h1>
+      <p className='mb-1'>I'm a web developer and a passionate learner.</p>
+      <p className='mb-1'>
+        If you'd like to learn more about me,{' '}
+        <Link
+          href='/about'
+          className={linkStyles}
+        >
+          click here
+        </Link>
+        !
+      </p>
+      <p className='mb-1'>
+        If you'd like to get in touch, you can submit a message through{' '}
+        <Link
+          href='/contact'
+          className={linkStyles}
+        >
+          this contact form
+        </Link>
+        .
+      </p>
+      <p>
+        Please excuse the appearance of my site while it's under construction.
+        Check back later to see what improvements I've made to this site.
+      </p>
     </PrimaryLayout>
   );
 };
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,10 +1,24 @@
 import PrimaryLayout from '@/components/PrimaryLayout';
+import Link from 'next/link';
 import React from 'react';
 
 const Index = () => {
   return (
     <PrimaryLayout>
-      <h1>Projects</h1>
+      <h1 className='text-2xl mb-3'>Projects</h1>
+      <p className='mb-1'>
+        Sorry, I'm still working on putting this page together.
+      </p>
+      <p>
+        For now, if you'd like to see some of my code, check out my{' '}
+        <Link
+          href='https://github.com/reinerb'
+          className='text-sky-700 dark:text-sky-300 visited:text-sky-600 dark:visited:text-sky-400 underline hover:no-underline'
+        >
+          GitHub
+        </Link>
+        .
+      </p>
     </PrimaryLayout>
   );
 };
