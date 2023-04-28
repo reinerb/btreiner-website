deaa23d2b8faf6a74882062a8255307e8d4b3a84
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-28 12:52:27 -0400

    Set contact API to post to Formspree

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,24 @@
+import axios from 'axios';
+
+const formspreeUrl = `https://formspree.io/f/${process.env.FORMSPREE_KEY}`;
+
 export default async (req, res) => {
-  res.status(200).json({ message: 'Success' });
+  if (req.method !== 'POST') {
+    res.status(500).json({ message: 'Only POST requests are valid' });
+    return;
+  }
+
+  const { formData } = req.body;
+
+  try {
+    await axios({
+      method: 'POST',
+      url: formspreeUrl,
+      message: formData,
+    });
+    res.status(200).json({ message: 'Success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Formspree server error' });
+  }
 };
