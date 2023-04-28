3ed990590032bd96ea7a318ff2caddf7acc9f746
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-28 15:12:14 -0400

    Move RecaptchaProvider to only contact page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,5 @@
 import '@/styles/globals.css';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-    >
-      <Component {...pageProps} />
-    </GoogleReCaptchaProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,13 +1,18 @@
 import ContactForm from '@/components/ContactForm';
 import PrimaryLayout from '@/components/PrimaryLayout';
 import React from 'react';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 const Contact = () => {
   return (
-    <PrimaryLayout>
-      <h1 className='text-4xl mb-4'>Contact Me</h1>
-      <ContactForm />
-    </PrimaryLayout>
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+    >
+      <PrimaryLayout>
+        <h1 className='text-4xl mb-4'>Contact Me</h1>
+        <ContactForm />
+      </PrimaryLayout>
+    </GoogleReCaptchaProvider>
   );
 };
 
