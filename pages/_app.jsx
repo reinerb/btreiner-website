2cd5c04d399a635f5af9ef52febfd09ec68b07dc
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-04-28 14:39:53 -0400

    Add react-google-recaptcha-v3 package

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+    >
+      <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
+  );
 }
